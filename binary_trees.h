9ce6c4e5ce8bf0cefa0209782aefae0013198e11
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-08-31 08:04:40 +0100

    20. Level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -53,4 +53,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /* BINARY_TREES_H */
