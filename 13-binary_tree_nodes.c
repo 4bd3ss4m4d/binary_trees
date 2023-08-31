7c90d389dc7c37970ae63b1d389d04afb3434b4f
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-08-31 22:00:21 +0100

    Nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least one child in a binary tree
+ * @tree: root of the tree to count nodes
+ * Return: number of nodes with at least one child, 0 if the tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
