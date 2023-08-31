0c62613dd1e3d4ef0fb3c24bcdea37e2e221d2f4
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-08-31 22:18:50 +0100

    Sibling

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 	if (!tree->left || !tree->right)
-		return (0);
+		return (-1);
 
 	return (binary_tree_is_full(tree->left) &&
 		binary_tree_is_full(tree->right));
