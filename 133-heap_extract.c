6916feb8f9dbe891c5307251015250eebb900797
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-09-01 01:43:40 +0100

    39. Heap - Extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,91 +1,163 @@
 #include "binary_trees.h"
 
-void heapify_down(heap_t *root);
+/**
+ * tree_height - Measures the height of a heap tree
+ * @tree: A pointer to the root of the heap tree
+ *
+ * Return: The height of the heap tree, or 0 if @tree is NULL
+ */
+size_t tree_height(const heap_t *tree)
+{
+	size_t h_left = 0, h_right = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left)
+	{
+		h_left = 1 + tree_height(tree->left);
+	}
+
+	if (tree->right)
+	{
+		h_right = 1 + tree_height(tree->right);
+	}
+
+	if (h_left > h_right)
+	{
+		return (h_left);
+	}
+
+	return (h_right);
+}
 
 /**
- * heap_extract - Removes the root node of a max-heap and restores the
- *		max-heap property
- * @root: double pointer to the root node of the heap
+ * tree_size_h - Measures the size of a binary tree
+ * @tree: A pointer to the root of the binary tree
  *
- * Return: Value of the extracted root node, or 0 if the heap is empty or
- *	 root is NULL
+ * Return: The size of the binary tree, or 0 if @tree is NULL
+ */
+size_t tree_size_h(const binary_tree_t *tree)
+{
+	size_t h_left = 0, h_right = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left)
+	{
+		h_left = 1 + tree_size_h(tree->left);
+	}
+
+	if (tree->right)
+	{
+		h_right = 1 + tree_size_h(tree->right);
+	}
+
+	return (h_left + h_right);
+}
+
+/**
+ * _order - Helper function to recursively find a node at a certain height
+ * @tree: A pointer to the current node being checked
+ * @node: A pointer to a pointer that will store the found node
+ * @height: The height at which to find the node
+ */
+void _order(heap_t *tree, heap_t **node, size_t height)
+{
+	if (!tree)
+	{
+		return;
+	}
+
+	if (!height)
+	{
+		*node = tree;
+	}
+
+	height--;
+
+	_order(tree->left, node, height);
+	_order(tree->right, node, height);
+}
+
+/**
+ * heapify - Ensures the max heap property is maintained from the given root
+ * @root: A pointer to the root of the heap
+ */
+void heapify(heap_t *root)
+{
+	heap_t *temp1, *temp2;
+	int vl;
+
+	if (!root)
+	{
+		return;
+	}
+
+	temp1 = root;
+
+	while (1)
+	{
+		if (!temp1->left)
+			break;
+		if (!temp1->right)
+			temp2 = temp1->left;
+		else
+		{
+			if (temp1->left->n > temp1->right->n)
+				temp2 = temp1->left;
+			else
+				temp2 = temp1->right;
+		}
+		if (temp1->n > temp2->n)
+			break;
+		vl = temp1->n;
+		temp1->n = temp2->n;
+		temp2->n = vl;
+		temp1 = temp2;
+	}
+}
+
+/**
+ * heap_extract - Extracts the root value of a max heap
+ * @root: A pointer to a pointer to the root of the max heap
+ *
+ * Return: The extracted root value, or 0 if @root is NULL or the
+ *         heap is empty
  */
 int heap_extract(heap_t **root)
 {
-	heap_t *nd;
 	int vl;
+	heap_t *heap_tree, *node_tree;
 
 	if (!root || !*root)
 	{
 		return (0);
 	}
-
-	vl = (*root)->n;
-	if (!(*root)->left && !(*root)->right)
+	heap_tree = *root;
+	vl = heap_tree->n;
+	if (!heap_tree->left && !heap_tree->right)
 	{
-		free(*root);
 		*root = NULL;
-
+		free(heap_tree);
 		return (vl);
 	}
 
-	nd = *root;
-	while (nd->left || nd->right)
-	{
-		if (!nd->right || (nd->left && nd->left->n >= nd->right->n))
-		{
-			nd->n = nd->left->n;
-			nd = nd->left;
-		}
-		else
-		{
-			nd->n = nd->right->n;
-			nd = nd->right;
-		}
-	}
+	_order(heap_tree, &node_tree, tree_height(heap_tree));
 
-	if (nd->parent->left == nd)
-		nd->parent->left = NULL;
+	heap_tree->n = node_tree->n;
+	if (node_tree->parent->right)
+		node_tree->parent->right = NULL;
 	else
-		nd->parent->right = NULL;
-
-	free(nd);
-	heapify_down(*root);
+		node_tree->parent->left = NULL;
+	free(node_tree);
+	heapify(heap_tree);
+	*root = heap_tree;
 
 	return (vl);
-}
-
-/**
- * heapify_down - Restores the max-heap property in a heap starting from a
- *		given node
- * @root: pointer to the root node of the heap
- *
- * Return: void
- */
-void heapify_down(heap_t *root)
-{
-	int vl;
-	heap_t *biggest;
-
-	while (1)
-	{
-		if (!root->left)
-		{
-			break;
-		}
-
-		if (!root->right || root->left->n > root->right->n)
-			biggest = root->left;
-		else
-			biggest = root->right;
-
-		if (root->n > biggest->n)
-			break;
-
-		vl = root->n;
-		root->n = biggest->n;
-		biggest->n = vl;
-
-		root = biggest;
-	}
 }
